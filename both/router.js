0111bc094af36e3e950a4868fc3263b5bee40649
Matt Newman <mattrobertnewman@gmail.com>
2015-03-05 17:34:12 -0700

    cleaned up

--- both/router.js
+++ both/router.js
@@ -115,34 +115,7 @@ Router.map(function() {
     fastRender: true
   });
   this.route('sync');
-  this.route('actionSheet');
-  this.route('backdrop');
-  this.route('forms', {
-    data: function () {
-      return {
-        post: Posts.find().fetch()[0]
-      };
-    }
-  });
-  this.route('headersFooters');
-  this.route('lists');
-  this.route('loading');
-  this.route('modal');
-  this.route('navigation');
-  this.route('navigation.one', {path: '/navigation/one'});
-  this.route('navigation.two', {path: '/navigation/two'});
-  this.route('navigation.three', {path: '/navigation/three'});
-  this.route('popover');
-  this.route('popup');
-  this.route('index');
-  this.route('slideBox');
-  this.route('tabs.one', {path: '/tabs/one', layoutTemplate: 'tabsLayout'});
-  this.route('tabs.two', {path: '/tabs/two', layoutTemplate: 'tabsLayout'});
-  this.route('tabs.three', {path: '/tabs/three', layoutTemplate: 'tabsLayout'});
-  this.route('tabs.four', {path: '/tabs/four', layoutTemplate: 'tabsLayout'});
   this.route('userAccounts');
-
-
   this.route('/meetingPrint/:_id', function() {
 
     meeting = meetingCollection.findOne(this.params._id);
