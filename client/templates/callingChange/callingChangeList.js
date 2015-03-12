1d7830c2044152f820a2596ee27bcadb6a1b793b
Matt Newman <mattrobertnewman@gmail.com>
2015-03-12 17:58:26 -0600

    template cleaner

---
+++ client/templates/callingChange/callingChangeList.js
@@ -0,0 +1,6 @@
+Template.callingChangeList.rendered = function() {
+  Session.set("memberSelectType", "");
+  Session.set("selectedCallingChangeMember", "");
+  Session.set("selectedCallingChangeCalling", "");
+  Session.set("selectedCallingChangeType", "");
+};
