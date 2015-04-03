9fb656852203bdc35dae7e9189777a6ce98b3230
Matt Newman <newmanmr@ldschurch.org>
2015-04-02 21:51:59 -0600

    new mapping options

--- client/templates/member/member.js
+++ client/templates/member/member.js
@@ -11,3 +11,26 @@ Template.member.rendered = function() {
   });
   */
 }
+
+Template.member.events({
+  'click [data-action=showAddressActionSheet]': function (event, template) {
+    var household = this;
+    IonActionSheet.show({
+      buttons: [
+        { text: "View Map"},
+        { text: 'View in Google Maps' },
+      ],
+      cancelText: 'Cancel',
+      cancel: function() {},
+      buttonClicked: function(index) {
+        if (index === 0) {
+          Router.go("/householdMap/" + household._id);
+        }
+        if (index === 1) {
+          window.location = "comgooglemaps://?center=40.765819,-73.975866&zoom=14&views=traffic";
+        }
+        return true;
+      }
+    });
+  }
+});
