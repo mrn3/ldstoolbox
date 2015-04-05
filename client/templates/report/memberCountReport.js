da552108885ac98be7afe3894c31239d07edde0b
Matt Newman <newmanmr@ldschurch.org>
2015-04-05 03:06:22 -0600

    cleaned up report

--- client/templates/report/memberCountReport.js
+++ client/templates/report/memberCountReport.js
@@ -1,16 +1,1 @@
-Template.memberCountReport.helpers({
-  user: function() {
-    return Meteor.user();
-  },
-  ldsAccountUserNotAuthenticated: function() {
-    if (Meteor.user() && Meteor.user().ldsAccount && Meteor.user().ldsAccount.updatedAt) {
-      var oneHourAgo = moment().subtract(1, "hours");
-      return oneHourAgo.isAfter(Meteor.user().ldsAccount.updatedAt);
-    } else {
-      return true;
-    }
-  },
-  reportData: function() {
-    return memberClientCollection.find();
-  }
-});
+
