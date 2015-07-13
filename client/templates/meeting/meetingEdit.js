0657de4b6574b5ff7f618a3607676f3189c1df5f
Matt Newman <mattrobertnewman@gmail.com>
2015-07-12 22:32:10 -0600

    added librarian

--- client/templates/meeting/meetingEdit.js
+++ client/templates/meeting/meetingEdit.js
@@ -16,8 +16,8 @@ Template.meetingEdit.helpers({
   userCanEditMeeting: function () {
     if (Meteor.user() && Meteor.user().callings) {
 
-      //bishop, counselors, executive secretary, ward clerk, membership clerk
-      var allowedCallingList = [4, 54, 55, 56, 57, 787];
+      //bishop, counselors, executive secretary, ward clerk, membership clerk, librarian
+      var allowedCallingList = [4, 54, 55, 56, 57, 787, 229];
       var userCallingList = Meteor.user().callings.reduce(
         function(total, calling){
           return total.concat(calling.positionTypeId);
